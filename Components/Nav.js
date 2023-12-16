973c5690b4d3c0abd4af0a58e754452494608887
Folson Fairley <folsonfairley20@gmail.com>
2023-12-16 09:18:02 -0600

    corrected componets folder

--- Components/Nav.js
+++ Components/Nav.js
@@ -1,14 +1,12 @@
 import html from "html-literal";
-export default links => html`
+export default Links => html`
   <nav>
     <i class="fas fa-bars"></i>
     <ul class="hidden--mobile nav-links">
-      ${links
-        .map(
-          link =>
-            `<li><a href="/${link.title}" title="${link.title}" data-navigo>${link.text}</a></li>`
-        )
-        .join("")}
+      ${Links.map(
+        Link =>
+          `<li><a href="/${Link.title}" title="${Link.title}" data-navigo>${Link.text}</a></li>`
+      ).join("")}
     </ul>
   </nav>
 `;
