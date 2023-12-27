9a81224af1254f41f6b95de073c0b8a910c7d20c
Folson Fairley <folsonfairley20@gmail.com>
2023-12-26 18:55:38 -0600

    corrected home page

--- components/views/Home.js
+++ components/views/Home.js
@@ -4,7 +4,7 @@ export default state => html`
     <p>
       Here at spotted we understand that sometimes pets lose their way. Our goal
       is to reunite families with their lost loved one. please take your time
-      and give us as mush information as possible about your lost one. Were here
+      and give us as much information as possible about your lost one. Were here
       to take as much stress out of this process as we can. We hope to help you
       find your lost one soon!
     </p>
