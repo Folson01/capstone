75bf0ec7a7e47f5f8233bd058eb9ed5b13015d4c
Folson Fairley <folsonfairley20@gmail.com>
2024-01-11 20:29:56 -0600

    complted corrections on API and page rendoring

--- components/views/Pets.js
+++ components/views/Pets.js
@@ -2,19 +2,19 @@ import html from "html-literal";
 
 export default state => html`
   <table id="Lostpets">
-    <!-- <tr>
+    <tr>
       <th>PetName</th>
       <th>Email</th>
       <th>Phone</th>
-      <th>msg</th>
+      <th>Message</th>
       <th>Approachable</th>
-    </tr> -->
-    ${state.pets
-      .map(pet => {
-        return `<<tr><td>${pet.PetName}<tr><td>${pet.Email}<tr><td>${pet.Phone}
-        <tr><td>${pet.msg}<tr><td>${pet.Approachable}<tr><td>`;
-      })
-      .join("")}
-    ;
+    </tr>
+      ${state.pets
+        .map(pet => {
+          return `<tr><td>${pet.PetName}</td><td>${pet.Email}</td><td>${pet.Phone}
+        </td><td>${pet.msg}</td><td>${pet.Approachable}</td></tr>`;
+        })
+        .join("")}
+    </tr>
   </table>
 `;
